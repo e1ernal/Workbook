06271ad99a566df3b86ff0f84d8158f5fe834fd5
unknown <>
2024-06-04 21:33:45 +0300

    added url for get list filter endpoint

--- Workbook/API/urls.py
+++ Workbook/API/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from API.views.filters import FilterListView
 from API.views.healf_check import HealthCheckView
 
 urlpatterns = [
     path('check/', HealthCheckView.as_view()),
+    path('filters/', FilterListView.as_view()),
 ]
