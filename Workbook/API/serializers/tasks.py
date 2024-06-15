0f556fe425357d38842ab56a06877f7928c7a29e
Dmitry <e1ernal@e1ernal-MacBook-Pro-14.local>
2024-06-15 15:02:29 +0300

    handle images

--- Workbook/API/serializers/tasks.py
+++ Workbook/API/serializers/tasks.py
@@ -1,18 +1,20 @@
 from rest_framework import serializers
 
 from API.serializers.filters import FilterListSerializer
+from API.serializers.images import ImageListSerializer
 from ModelsApp.models import Task
 
 
 class TaskListSerializer(serializers.ModelSerializer):
     filters = FilterListSerializer(many=True)
+    images = ImageListSerializer(many=True)
 
     class Meta:
         model = Task
         fields = (
             'id',
             'text',
-            # 'text_image',
+            'images',
             'solution',
             'answer',
             'filters'
